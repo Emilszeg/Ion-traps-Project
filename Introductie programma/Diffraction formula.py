1a61f1fbff32d346168e36d7aa59866d540f4946
Emils Zegers <emilszegers.ez@gmail.com>
2023-06-06 12:08:15 +0200

    Added a function for the length of the first order

--- Introductie programma/Diffraction formula.py
+++ Introductie programma/Diffraction formula.py
@@ -1,10 +1,22 @@
-from math import sin, cos, tan, pi, sqrt, atan,asin
+#%%
+from math import sin, cos, tan, pi, sqrt, atan,asin, radians, degrees
 
 def formula(incomming_angle, order, lambdaa, a):
-    return asin(order * lambdaa / (a*sin(incomming_angle)))
+    return degrees(asin((order * lambdaa - a*sin(radians(incomming_angle)))/a))
 
-a = 1000
-lambdaa = 0.0000005
+a = 7.4982*10**(-7)
+lambdaa = 600*10**(-9)
 invoer = float(input("Incomming angle: "))
 order = int(input("Order: "))
 print(formula(invoer, order, lambdaa, a))
+#%%
+print(400*10**(-9)/(sin(radians(45)) - sin(radians(10))))
+
+#%%
+def length_first_order(incomming_angle, a, distance_screen):
+    red_angle = -formula(incomming_angle, 1, 600*10**(-9), a)
+    blue_angle = -formula(incomming_angle, 1, 380*10**(-9), a)
+    return distance_screen * tan(radians(incomming_angle-red_angle)) - distance_screen * tan(radians(incomming_angle-blue_angle))
+
+#%%
+print(length_first_order(45, 7.4982*10**(-7), 0.111))
